47e518074780bd6931f94d52757687fb6203bf8e
loic lefloch <l.lefloch.91@gmail.com>
2019-01-03 19:12:27 +0100

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,59 @@
+const proxy = require("http-proxy-middleware")
+
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+
+  // https://www.gatsbyjs.org/blog/2018-12-17-turning-the-static-dynamic/
+  // for avoiding CORS while developing Netlify Functions locally
+  // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
+  developMiddleware: app => {
+    app.use(
+      "/.netlify/functions/",
+      proxy({
+        target: "http://localhost:9000",
+        pathRewrite: {
+          "/.netlify/functions/": "",
+        },
+      })
+    )
+  },
+  
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+
+    // https://www.gatsbyjs.org/blog/2018-12-17-turning-the-static-dynamic/
+    {
+      resolve: `gatsby-plugin-create-client-paths`,
+      options: { prefixes: [`/app/*`] },
+    },
+
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
---
+++ src/lambda/hello.js
@@ -0,0 +1,12 @@
+// https://www.gatsbyjs.org/blog/2018-12-17-turning-the-static-dynamic/
+// For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
+export function handler(event, context, callback) {
+  console.log("queryStringParameters", event.queryStringParameters)
+  callback(null, {
+    // return null to show no errors
+    statusCode: 200, // http status code
+    body: JSON.stringify({
+      msg: "Hello, World! " + Math.round(Math.random() * 10),
+    }),
+  })
+}
